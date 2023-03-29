80cbdabd0c84de28534fd429bdc917ff9ca58f6c
mike <mgalakos@gmail.com>
2023-03-29 20:24:06 +0300

    basic functionality

--- rawdb.js
+++ rawdb.js
@@ -1,1 +1,44 @@
-console.log('rawdb')
+import { load } from './lib/load-data.js'
+import { httpMiddleware } from './lib/http-middleware.js'
+
+
+const ROOT_DIR = 'content'
+
+export const { state, settings } = await load(ROOT_DIR)
+// console.debug(JSON.stringify({ state, settings }, null, 2))
+
+export function rawDbHttpMiddleware() {
+  return httpMiddleware(state, settings)
+}
+
+
+
+
+
+// references
+
+// console.debug(JSON.stringify(state['instructors'].map(o=>o.name + ',' + o.sort),null,2))
+
+
+
+
+
+export function getNextItem(collection, currentItem) {
+  if (!state[collection]) return null
+  const currentIndex = state[collection].findIndex(o => o._id === currentItem._id)
+  if (currentIndex < state[collection].length - 1) {
+    return state[collection][currentIndex + 1]
+  } else {
+    return null
+  }
+}
+
+export function getPrevItem(collection, currentItem) {
+  if (!state[collection]) return null
+  const currentIndex = state[collection].findIndex(o => o._id === currentItem._id)
+  if (currentIndex > 0) {
+    return state[collection][currentIndex - 1]
+  } else {
+    return null
+  }
+}
