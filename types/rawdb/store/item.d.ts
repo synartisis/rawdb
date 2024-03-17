09a4b22db7fbe1ef8b671aabe1bb9e54ef433584
mike <mgalakos@gmail.com>
2024-03-17 11:49:56 +0200

    fix types

--- types/rawdb/store/item.d.ts
+++ types/rawdb/store/item.d.ts
@@ -1,6 +1,6 @@
 export function loadItem(collection: Collection, filename: string, loadLazyProperties: boolean, includeBodySource?: boolean): Promise<CollectionItem | undefined>;
 export function applyItemChanges(collection: Collection, filename: string, changes: import("../../types.js").ChangeSet): Promise<CollectionItem>;
-export function saveItemOnFile(collection: Collection, item: CollectionItem, type: ItemType): Promise<void>;
+export function saveItemOnFile(collection: Collection, item: CollectionItem, type: ItemType, filename: string): Promise<void>;
 export type Collection = import('../../types.js').Collection;
 export type CollectionItem = import('../../types.js').CollectionItem;
 export type ItemType = import('../../types.js').ItemType;
