10f0ee363d3ee61d10cb9017d4affb20347e20f5
mike <mgalakos@gmail.com>
2024-03-17 11:48:47 +0200

    store.saveItem

--- types/rawdb/store/item.d.ts
+++ types/rawdb/store/item.d.ts
@@ -1,5 +1,7 @@
 export function loadItem(collection: Collection, filename: string, loadLazyProperties: boolean, includeBodySource?: boolean): Promise<CollectionItem | undefined>;
 export function applyItemChanges(collection: Collection, filename: string, changes: import("../../types.js").ChangeSet): Promise<CollectionItem>;
+export function saveItemOnFile(collection: Collection, item: CollectionItem, type: ItemType): Promise<void>;
 export type Collection = import('../../types.js').Collection;
 export type CollectionItem = import('../../types.js').CollectionItem;
+export type ItemType = import('../../types.js').ItemType;
 export type ChangeSet = import('../../types.js').ChangeSet;
